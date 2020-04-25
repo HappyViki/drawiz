191000ec2e482bae8abaa23ee3efc18bca14cac3
Veronica Eulenberg <veronica.eulenberg@gmail.com>
2020-04-25 15:00:14 -0600

    Updates

---
+++ script.js
@@ -0,0 +1,22 @@
+var el = document.getElementById('homework');
+var el2 = document.getElementById('review');
+var vanilla = new Croppie(el, {
+		enableOrientation: true,
+		enableExif: true,
+    viewport: {
+			width: 100,
+			height: 100,
+		},
+    boundary: { width: 300, height: 300 },
+    showZoomer: true,
+    enableOrientation: true
+});
+vanilla.bind({
+    url: 'antoine-dautry-05A-kdOH6Hw-unsplash.jpg',
+    orientation: 4
+});
+//on button click
+vanilla.result('base64').then(function(blob) {
+    el2.src = blob;
+		this.Content = this.sanitizer.bypassSecurityTrustUrl(result);
+});
