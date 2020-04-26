eb8080a98f4c310cd6cf99594d5762a410a4e25e
Veronica Eulenberg <veronica.eulenberg@gmail.com>
2020-04-26 03:08:32 -0600

    everything works now

---
+++ dist/script.js
@@ -0,0 +1,110 @@
+const imgUrl = 'antoine-dautry-05A-kdOH6Hw-unsplash.jpg'
+const quiz = [
+	{
+		id: "questionone",
+		name: "Question One",
+		base64Data: ""
+	},
+	{
+		id: "questiontwo",
+		name: "Question Two",
+		base64Data: ""
+	},
+	{
+		id: "questionthree",
+		name: "Question Three",
+		base64Data: ""
+	}
+]
+let imageReady = true
+
+const quizQuestions = document.getElementById('quizQuestions')
+const uploadQuestions = document.getElementById('uploadQuestions')
+
+quizQuestions.innerHTML = quiz.map(
+	question => `<div class="quiz-question">${question.name}</div>`
+).join("")
+
+uploadQuestions.innerHTML = quiz.map(
+	question => `<div class="upload-question">
+		<label for="name">
+			${question.name}
+		</label>
+		<input
+			type="text"
+			name="${question.id}"
+			id="${question.id}"
+			required
+			hidden
+		/>
+		<img src="" alt="Click here to crop answer" />
+	</div>`
+).join("")
+
+const croppie = new Croppie(
+	document.getElementById('croppie'),
+	{
+		enableOrientation: true,
+		enableExif: true,
+		viewport: {
+			width: 100,
+			height: 100,
+		},
+		boundary: { width: 300, height: 300 },
+		showZoomer: true,
+		enableOrientation: true
+	}
+);
+croppie.bind({
+		url: imgUrl,
+		orientation: 4
+});
+
+const selectFile = file => {
+	// https://gist.github.com/felixzapata/3684117
+	// load an image from an input file into canvas tag
+	var reader = new FileReader();
+	// Read in the image file as a data URL.
+	reader.readAsDataURL(file);
+	reader.onload = function(e){
+		imageReady = false
+		if( e.target.readyState == FileReader.DONE) {
+			console.log(e.target.result);
+			imageReady = true
+			croppie.bind({
+			    url: e.target.result,
+			    orientation: 4
+			});
+		}
+	}
+}
+
+const saveCrop = question => {
+	croppie.result(
+		{
+			type:'base64',
+			format:'jpeg'
+		})
+		.then(
+			data => {
+				if (!imageReady) return
+				question.classList.add("answered")
+				question.parentNode.querySelector("input").value = data
+				question.parentNode.querySelector("img").src = data
+				question.parentNode.querySelector("img").alt = "answer"
+			}
+		);
+}
+
+document.querySelectorAll("#file").forEach(
+	question => question.addEventListener(
+		'change',
+		function() {selectFile(this.files[0])}
+	)
+)
+document.querySelectorAll("#uploadQuestions img").forEach(
+	question => question.addEventListener(
+		'click',
+		() => saveCrop(question)
+	)
+)
--- exam.js
+++
@@ -1,5 +0,0 @@
-const exam = [
-	"Graph y = 5x - 2",
-	"Graph y = x2 - 5 + 8",
-	"Graph a cool wave"
-]
